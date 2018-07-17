eeeaebd85b3a4af60574502a1f74bb34a6bc949d
radio1988 <liruiradiant@gmail.com>
2018-07-17 11:47:24 -0400

    organize_files

--- perl/cuffmerge_anno/gtf_extract_gene_id_gene_name_table.pl
+++ perl/cuffmerge_anno/gtf_extract_gene_id_gene_name_table.pl
@@ -1,5 +1,9 @@
 use strict; use warnings;
 
+warn "extract gene_id to gene_name table for DESeq annotation";
+warn "also does GTF integrety check: valid gene_id, gene_name, trans_id\n";
+warn "need quotes in ID/Names\n";
+
 my%id2name;
 while(<>){
     if (/^#/){next}
@@ -11,8 +15,11 @@ while(<>){
 
     if ($attribute =~ /gene_name/ && $attribute =~ /gene_id/){
         my($gene_id) = $attribute =~ /gene_id "([^"]+)"/;
-        if (!$gene_id){warn $_}
         my($gene_name) = $attribute =~ /gene_name "([^"]+)"/;
+        my($trans_id) = $attribute =~ /transcript_id "([^"]+)"/;
+        if (!$gene_id || $gene_id eq '-'){warn "NO GENE_ID: $_"}
+        if (!$gene_name || $gene_name eq '-'){warn "NO GENE_ID: $_"}
+        if (!$trans_id || $trans_id eq '-'){warn "NO GENE_ID: $_"}
         $id2name{$gene_id} = $gene_name;
     }
 
