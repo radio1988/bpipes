45a26da3f51cc2ab6bbfa30daad325fdb0affff4
rui <liruiradiant@gmail.com>
2018-08-30 16:01:14 -0400

    better

--- perl/cuffmerge_anno/gtf_extract_gene_id_gene_name_table.pl
+++ perl/cuffmerge_anno/gtf_extract_gene_id_gene_name_table.pl
@@ -1,11 +1,17 @@
 use strict; use warnings;
 
-warn "extract gene_id to gene_name table for DESeq annotation";
-warn "also does GTF integrety check: valid gene_id, gene_name, trans_id\n";
-warn "need quotes in ID/Names\n";
+print "input should be ENSEMBL format GTF\n"; 
+print "extract gene_id to gene_name table for DESeq annotation\n";
+print "also does GTF integrety check: valid gene_id, gene_name, trans_id\n";
+print "need quotes in ID/Names\n";
+
+die ("usage: gtf_extract_gene_id_gene_name_table.pl   ensembl.gtf   out.anno.txt") unless @ARGV==2;
+
+open(IN,  $ARGV[0]) or die;
+open(OUT, ">$ARGV[1]") or die;
 
 my%id2name;
-while(<>){
+while(<IN>){
     if (/^#/){next}
 
     $_ =~ s/[\r\n]//g;
@@ -13,9 +19,9 @@ while(<>){
     my ($seqname, $source, $feature, $start, $end, $score, $strand, $frame, $attribute) = split(/\t/, $_, 9);
 
 
-    if ($attribute =~ /gene_name/ && $attribute =~ /gene_id/){
-        my($gene_id) = $attribute =~ /gene_id "([^"]+)"/ || $attribute =~ /gene_id ([^;]+);/;
-        my($gene_name) = $attribute =~ /gene_name "([^"]+)"/ || $attribute =~ /gene_name ([^;]+);/;
+    if ($attribute =~ /gene_id/){
+        my($gene_id) = $attribute =~ /gene_id "([^"]+)"/;
+        my($gene_name) = $attribute =~ /gene_name "([^"]+)"/;
         if (!$gene_id || $gene_id eq '-'){warn "NO GENE_ID: $gene_id; $_"}
         if (!$gene_name || $gene_name eq '-'){warn "NO Gene_name: $gene_name; $_"}
         $id2name{$gene_id} = $gene_name;
@@ -30,5 +36,5 @@ while(<>){
 }
 
 foreach my$id (sort keys %id2name){
-    print "$id\t$id2name{$id}\n";
+    print OUT "$id\t$id2name{$id}\n";
 }
