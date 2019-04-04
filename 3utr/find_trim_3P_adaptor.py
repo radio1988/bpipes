6fa07488e40919603c1585366bd04638bb27301e
radio1988 <liruiradiant@gmail.com>
2019-04-04 08:39:23 -0400

    update

--- 3utr/find_trim_3P_adaptor.py
+++ 3utr/find_trim_3P_adaptor.py
@@ -44,38 +44,55 @@ def search_tailing_as(seq):
 
 
 
-
 # Note:
 print('''
->> Desc:
-Read R1 and R2 in PE mode
+<find_trim_3P_adaptor.py> PE mode
+Help Information:
 
-Filtering
+Example: 
+python3 find_trim_3P_adaptor.py R1 R2 -s 61.5 -t 1
+
+>> Workflow
+> Read R1 and R2 in PE mode
+
+> Filtering
 1. Contains adaptor in the beginning of R2: If adaptor similarity > 61.5% for the beginning 26 bp in R2
 2. R2 ends with 2-39 terminal A's 
 2. R1 starts with leading T's in the beginning
 
-Trimming
+> Trimming
 1. removed first 26bp in R2 (always)
-2. removed ending As in R2 (optional by -t)
-3. removed starting Ts in R1 (optinoal by -t)
+2. removed ending As in R2 (optional by -t 1)
+3. removed starting Ts in R1 (optinoal by -t 1)
+
+>> Update Note (new to old):
+- added progress info
+- remove pairs that are too short after trimming
+- print args
+
+>> Todo: 
+1. More flexible
+
 ''')
+
+
+
 
 # Parameters
 args = parse_args(sys.argv[1:])
-file1 = args.file1
-file2 = args.file2
 
-out1=file1.replace(".fastq.gz", "")
+out1=args.file1.replace(".fastq.gz", "")
 out1=out1+".flt.fastq.gz"
 
-out2=file2.replace(".fastq.gz", "")
+out2=args.file2.replace(".fastq.gz", "")
 out2=out2+".flt.fastq.gz"
 
 adaptor="GTTCAGAGTTCTACAGTCCGACGATC"
 min_similarity=float(args.min_similarity) #61.5  # 16/26
 
-print("inputs:", file1, file2)
+print(">>> Began:")
+print("cmd: ", args)
+print("inputs:", args.file1, args.file2)
 print("similarity", min_similarity)
 print("outputs:", out1, out2)
 
@@ -87,8 +104,7 @@ write_x = gzip.open(out1, 'wt')
 write_y = gzip.open(out2, "wt")
 
 # Work
-# Work
-with gzip.open(file1, "rt") as f1, gzip.open(file2, "rt") as f2: #rt for text mode
+with gzip.open(args.file1, "rt") as f1, gzip.open(args.file2, "rt") as f2: #rt for text mode
     i=0
     o=0
     for x, y in zip(f1, f2):  # read two files parrallel 
@@ -98,7 +114,11 @@ with gzip.open(file1, "rt") as f1, gzip.open(file2, "rt") as f2: #rt for text mo
         residue = i % 4
         if residue == 0:
             residue = 4
-        residue = residue -1      
+        residue = residue - 1      
+
+        # progress report
+        if residue == 4 and i//4 % 1000000 == 1: 
+            print("{} M pairs processed".format(i//4000000))
         
         # clean up
         x = x.strip()
@@ -109,7 +129,6 @@ with gzip.open(file1, "rt") as f1, gzip.open(file2, "rt") as f2: #rt for text mo
         out_x[residue] = x
         out_y[residue] = y
     
-        
         # check R1, R2 match
         if  residue == 0:  # ID
             if not x.split()[0] == y.split()[0]:
@@ -128,10 +147,10 @@ with gzip.open(file1, "rt") as f1, gzip.open(file2, "rt") as f2: #rt for text mo
             (leading_ts, len_leading_ts) = search_leading_ts(x)
 
 
-        # Output
+        # Output at line 4
         if residue == 3 and adap_qual=="Good" and leading_ts=="Good" and tailing_as=="Good":
-#             print ("read number", i//4, "\t similarity:{}\tlen_R1_ts:{}\tlen_R2_as:{}\n".
-#                    format(similarity,len_leading_ts, len_taila))
+            # print ("read number", i//4, "\t similarity:{}\tlen_R1_ts:{}\tlen_R2_as:{}\n".
+            #        format(similarity,len_leading_ts, len_taila))
             
             # trim R2 adaptor
             out_y[1] = out_y[1][26:]
@@ -145,28 +164,23 @@ with gzip.open(file1, "rt") as f1, gzip.open(file2, "rt") as f2: #rt for text mo
                 out_x[1] = out_x[1][len_leading_ts:]
                 out_x[3] = out_x[3][len_leading_ts:]
             
-            # next if nothing left
-            if (len(out_x[1])<13 or len(out_y[1])<13):
-#                 print ("too short after trimming")
-                next
+            # if not too short after trimming
+            if not (len(out_x[1])<13 or len(out_y[1])<13):
+                # output
+                write_x.write("\n".join(out_x))
+                write_x.write("\n")
+                write_y.write("\n".join(out_y))
+                write_y.write("\n")
+                o += 1
+                # output preview
+                # print("R1:")
+                # print("\n".join(out_x))
+                # print("\n")
+                # print("R2")
+                # print("\n".join(out_y))
+                # print("\n")
 
-            
-#             # output preview
-#             print("R1:")
-#             print("\n".join(out_x))
-#             print("\n")
-#             print("R2")
-#             print("\n".join(out_y))
-#             print("\n")
-            
-            # output
-            write_x.write("\n".join(out_x))
-            write_x.write("\n")
-            write_y.write("\n".join(out_y))
-            write_y.write("\n")
-            o += 1
 
-write_x.close()
-write_y.close()
 
-print(">>>Finished, there are {} read_pairs passed filter, trimming".format(o))
+
+print(">>>Finished, there are {}/{} read_pairs passed".format(o, i//4))
