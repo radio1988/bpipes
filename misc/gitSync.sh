61d11f06e6db52d8de6fca68baa492aff1a128bd
rui <liruiradiant@gmail.com>
2019-04-05 14:45:05 -0400

    format

--- misc/gitSync.sh
+++ misc/gitSync.sh
@@ -1,3 +1,7 @@
-printf "git push on mac; pull on hpcc"
+printf ">>>git push on mac; pull on hpcc\n"
+
+printf "\n\n\n>>>Pushing\n"
 git push
+
+printf "\n\n\n>>>Pulling on HPCC\n"
 ssh -t rl44w@ghpcc06.umassrc.org 'cd github/bpipes; git pull'
