8c1474645c5832240b1fea0c328e8505f702eb45
radio1988 <liruiradiant@gmail.com>
2019-05-08 02:11:09 -0400

    stop LSF emails, but keep log

--- snakemake/rnaseq/submit.sh
+++ snakemake/rnaseq/submit.sh
@@ -1,3 +1,3 @@
 nohup snakemake --jobs 999 --latency-wait 14400 \
---cluster 'bsub -q short -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' &
+--cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' &
 
