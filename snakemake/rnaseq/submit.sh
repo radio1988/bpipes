0a228f740559dce009bb034da4bd86acf004f201
radio1988 <liruiradiant@gmail.com>
2019-05-09 14:41:05 -0400

    temp

--- snakemake/rnaseq/submit.sh
+++ snakemake/rnaseq/submit.sh
@@ -1,3 +1,5 @@
 nohup snakemake --jobs 999 --latency-wait 14400 \
 --cluster 'bsub -q short -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' &
 
+snakemake --report report.html
+
