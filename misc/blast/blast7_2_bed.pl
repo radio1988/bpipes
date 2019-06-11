02898eb089dded01d0571058e8dcc8c1b7b93d7f
rui <liruiradiant@gmail.com>
2019-06-11 13:57:41 -0400

    debugged

--- misc/blast/blast7_2_bed.pl
+++ misc/blast/blast7_2_bed.pl
@@ -1,11 +1,12 @@
 use strict; use warnings;
 
 while(<>){
+    chomp;
     next if /^#/;
     my@F=split"\t", $_;
     if (int($F[8]) > int($F[9])) {
-        print join"\t", $F[1],$F[9],$F[8],$F[0],$F[11],"-"
+        print join"\t", $F[1],$F[9],$F[8],$F[0],$F[11],"-\n"
     }else{
-        print join"\t", $F[1],$F[8],$F[9],$F[0],$F[11],"+"
+        print join"\t", $F[1],$F[8],$F[9],$F[0],$F[11],"+\n"
     }
 }
