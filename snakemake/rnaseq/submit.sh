7bd933e9a63a5ac246df35f4ac6e2720507bb5ee
radio1988 <liruiradiant@gmail.com>
2019-06-24 15:14:46 -0400

    bug fixed

--- snakemake/rnaseq/submit.sh
+++ snakemake/rnaseq/submit.sh
@@ -1,5 +1,11 @@
+# star_indexing
+nohup snakemake star_idx --jobs 999 --latency-wait 14400 \
+--cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' &
+
+# run
 nohup snakemake --jobs 999 --latency-wait 14400 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' &
 
+# report
 snakemake --report report.html
 
