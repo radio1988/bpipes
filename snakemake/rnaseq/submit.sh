9400fddc6052f2de67790aa1497695d1500bc7e1
radio1988 <liruiradiant@gmail.com>
2019-07-05 11:00:23 -0400

    auto create report.html

--- snakemake/rnaseq/submit.sh
+++ snakemake/rnaseq/submit.sh
@@ -1,5 +1,2 @@
 nohup snakemake --jobs 999 --latency-wait 604800 \
---cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 168:00' &
-
-snakemake --report report.html
-
+--cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 168:00' && snakemake --report report.html &
