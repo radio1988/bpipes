1f71d865ec1e227a1f1c8031ba9fc1e251e14ead
radio1988 <liruiradiant@gmail.com>
2020-02-11 09:08:44 -0500

    both atac and chip, change config.yaml; remove chrM from markDup, Genrich for ATAC mode; MACS2 added; --use-conda for samtools1.9; added benchmark for bwa; -k mode;

--- snakemake/chip_atac/submit.sh
+++ snakemake/chip_atac/submit.sh
@@ -1,5 +1,8 @@
-nohup snakemake --jobs 999 --latency-wait 86400 \
+nohup snakemake -k --jobs 999 --latency-wait 86400 \
 --cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 24:00' && snakemake --report report.html &
 
-nohup snakemake --jobs 999 --latency-wait 14400 \
+nohup snakemake --use-conda -k --jobs 999 --latency-wait 5 \
 --cluster 'bsub -q short -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 4:00' && snakemake --report report.html &
+
+# Notes
+# -k: keep going
