ecbef12670ff0f2ead7b9325c2b6ab6c2e8372de
radio1988 <liruiradiant@gmail.com>
2020-12-17 17:43:12 -0500

    updated env.yaml for damid

--- snakemake/DamID/submit.sh
+++ snakemake/DamID/submit.sh
@@ -1,10 +1,7 @@
 #nohup bash submit.sh &
-# bsub -W 48:00 -q long 'source activate osr; bash submit.sh &> submit.log '
-
-snakemake -k --jobs 999 --latency-wait 600 \
---cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 336:00'
+# bsub -W 48:00 -q long 'source activate damid; bash submit.sh &> submit.log '
+source activate damid
+snakemake -k -p --jobs 999 --latency-wait 20 \
+--cluster 'bsub -q long -o lsf.log -R "rusage[mem={params.mem}]" -n {threads} -R span[hosts=1] -W 24:00'
 
 snakemake -j 1 --report report.html
-
-# Notes
-# -k: keep going
