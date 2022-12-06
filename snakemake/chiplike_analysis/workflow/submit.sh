3f3df8d02dc00f43c40dd9ad754c57d2b93f2b19
radio1988 <liruiradiant@gmail.com>
2022-12-06 17:10:27 -0500

    submit time from 24h to 144h, as meme can be slow

--- snakemake/chiplike_analysis/workflow/submit.sh
+++ snakemake/chiplike_analysis/workflow/submit.sh
@@ -1,23 +1,25 @@
 #nohup bash submit.sh &
-# bsub -W 48:00 -q long -R select[rh=8] 'source activate snakemake; bash workflow/submit.sh &> submit.log '
+# bsub -W 48:00 -q long -R select[rh=8] 'source activate snakemake7; bash workflow/submit.sh &> submit.log '
 
 source activate snakemake7
 
+
+## use conda
 snakemake -kp --ri \
 --notemp \
 --use-conda  --conda-prefix "~/anaconda3/envs/chip" \
 --conda-frontend mamba \
---use-envmodules \
 --ri  --restart-times 1 \
 --jobs 99  --latency-wait 20 \
---cluster 'bsub -q long -R select[rh=8] -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 24:00'
+--cluster 'bsub -q long -R select[rh=8] -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 144:00'
 
+
+## for meme, use module
 snakemake -k -p --ri \
 --notemp \
---use-conda  --conda-prefix "~/anaconda3/envs/chip" \
 --use-envmodules \
 --ri  --restart-times 1 \
 --jobs 99  --latency-wait 20 \
---cluster 'bsub -q long -R select[rh=6] -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 24:00'
+--cluster 'bsub -q long -R select[rh=6] -o lsf.log -R "rusage[mem={resources.mem_mb}]" -n {threads} -R span[hosts=1] -W 144:00'
 
 snakemake -j 1 --report report.html
