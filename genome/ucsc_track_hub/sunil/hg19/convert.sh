4580c0577c6cd7c640ae7649271f846e62ff271f
rui <liruiradiant@gmail.com>
2023-05-09 14:04:19 -0400

    lincRNA.bb

--- genome/ucsc_track_hub/sunil/hg19/convert.sh
+++ genome/ucsc_track_hub/sunil/hg19/convert.sh
@@ -1,1 +1,8 @@
-~/bin/ucsc_tools/gtfToGenePred lincRNA.gff3 lincRNA.genePred
+gffread lincRNA.gff3 -T -o lincRNA.gtf # bioconda cufflinks
+
+rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/ ucsc_tools/
+ ~/bin/ucsc_tools/gtfToGenePred lincRNA.gtf lincRNA.genePred
+ ~/bin/ucsc_tools/genePredToBigGenePred lincRNA.genePred lincRNA.BigGenePred
+curl -O https://genome.ucsc.edu/goldenPath/help/examples/bigGenePred.as
+~/bin/ucsc_tools/bedToBigBed -type=bed12+8 -tab -as=bigGenePred.as lincRNA.BigGenePred http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes lincRNA.bb
+
